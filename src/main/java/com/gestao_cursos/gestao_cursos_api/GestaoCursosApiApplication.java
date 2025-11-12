895c1e1708712f965a02b39d74d7519081a55fa9
Gabriel Soares Segatto <gabriel.ssegatto@dinfor.uniceub>
2025-11-12 10:46:30 -0300

    first commit

---
+++ src/main/java/com/gestao_cursos/gestao_cursos_api/GestaoCursosApiApplication.java
@@ -0,0 +1,13 @@
+package com.gestao_cursos.gestao_cursos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoCursosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoCursosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestao_cursos/gestao_cursos_api/GestaoCursosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao_cursos.gestao_cursos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoCursosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
